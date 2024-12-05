9d517ac0524cde2951514432f51a1ce6a7853d3f
Dave Cuthbert <daveforprojects@gmail.com>
2024-12-04 22:35:14 -0500

    more mutability (#9)
    * Chapter 2
    
    * Chapter 3
    
    * mutability
    
    * mutability
    
    * mutability
    
    * force commit

--- py-for-everyone/ch08.extra.01.py
+++ py-for-everyone/ch08.extra.01.py
@@ -28,14 +28,17 @@ print('lists original')
 for l in lsts1:
     print(f"{id(l)}:   {l}")
 print('')
+var_list2.append('f')
+print(f"{id(var_list2)}:   {var_list2}")
+print('')
 
 print('')
 
 # Update
-var_str1 = 'abcd'                  # Creates new variable w/ same name
-var_str2 = 'abcd'                 
-var_int1 = 1234                    # Creates new variable w/ same name
-var_int2 = 1234                    
+var_str1 = 'abc' + 'd'             # Creates new variable w/ same name
+var_str2 = 'abc' + 'd'                  
+var_int1 = 123 + 1                 # Creates new variable w/ same name
+var_int2 = 123 + 1            
 var_list1 = ['a', 'b', 'c', 'd']   # Creates new variable w/ same name
 var_list2 = ['a', 'b', 'c', 'd']   # Creates new variable w/ same name
 var_list3 = var_list2
@@ -59,6 +62,9 @@ print('')
 print('lists updated')
 for l in lsts2:
     print(f"{id(l)}:   {l}")
+print('')
+var_list2.append('f')
+print(f"{id(var_list2)}:   {var_list2}")
 print('')
 
 print('')
