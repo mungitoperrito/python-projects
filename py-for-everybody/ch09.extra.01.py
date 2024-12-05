5272af6f52bfaa0c428c757a6f8f3a2a00afb25a
Dave For Projects <daveforprojects@gmail.com>
2024-12-05 12:02:37 -0500

    Dictionary helpers

--- py-for-everybody/ch09.extra.01.py
+++ py-for-everybody/ch09.extra.01.py
@@ -3,9 +3,10 @@
 d = dict()
 d_keys = ['aa', 'bb', 'cc', 'aa', 'aa', 'bb', 'dd']
 
+# Use dictionary.get(key, default) to set a default value
 for dk in d_keys:
-    # Use dictionary.get(key, default) to set a default value
     d[dk] = d.get(dk, 0) + 1
 
+# use dictionary.items() and multiple-assignment
 for k,v in d.items():
     print(f"key: {k}  value: {v}")
