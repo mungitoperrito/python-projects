e474feee17e24b8ac4b8c1d896e71d33903059b5
Dave For Projects <daveforprojects@gmail.com>
2024-12-06 21:30:27 -0500

    ch07 extra

--- py-for-everybody/cho07.extra.01.py
+++ py-for-everybody/cho07.extra.01.py
@@ -9,10 +9,21 @@ DOESNT_EXIST = 'not-a-file.txt'
 # fh_02 = open(DOESNT_EXIST)  # Blows up
 # print(fh_02)
 
-# try block
+# # try block
+# try:
+#     fh_02 = open(DOESNT_EXIST)
+#     print(fh_02)
+# except Exception as e:
+#     print("File not found")
+#     print(e)
+
+# Command line arg for filename
+import sys
+FILENAME = sys.argv[1]
+
 try:
-    fh_02 = open(DOESNT_EXIST)
-    print(fh_02)
+    fh_03 = open(FILENAME)
+    print(fh_03)
 except Exception as e:
-    print("File not found")
+    print(f"Can't open file: {FILENAME}")
     print(e)
