779b836038065c8ea928550e0d4675fe5e155eeb
Dave For Projects <daveforprojects@gmail.com>
2024-12-17 11:44:38 -0500

    ch03 args

--- test-automation-tau/python-tutorial/ch03-keyword-args.py
+++ test-automation-tau/python-tutorial/ch03-keyword-args.py
@@ -17,11 +17,51 @@
 # user_info("Name03", 35, "City03")
 # user_info(city="City04", name="Name04", age=42)
 
-# Example 03
-# *args
-def addition(*args):
-    print(sum(args))
+# # Example 03
+# # *args
+# def addition(*args):
+#     print(sum(args))
 
-addition()            # NOTE: Converts no arg to 0
-addition(1,2)
-addition(1,2,3)
+# addition()            # NOTE: Converts no arg to 0
+# addition(1,2)
+# addition(1,2,3)
+
+# # Example 04
+# # **kwargs
+# def user_info(**kwargs):
+#     print(kwargs)
+
+# user_info()            # NOTE: prints empty dict
+# user_info(a="AA")
+# user_info(a="AA", b="BBB", g=20)
+
+# Example 05
+# **kwargs, *args, pos
+# def user_info(fname, lname="LN01", *args, **kwargs):
+def user_info(fname, lname, *args, **kwargs):
+    print("pos01: {} pos02: {} args: {}  kwargs: {}".format(
+        fname, lname, args, kwargs
+        )
+    )
+
+# user_info("FN01") # Too few args
+user_info(lname="LN02", fname="FN02")  # Arg order changes
+user_info("LN03", "FN03", 2, 4, 6)       # Postional args before keyword
+user_info("LN04", "FN04", 2, 4, 6, "AA", "BB", "CC") # args eats all, no kword
+user_info("LN04", "FN04", 2, 4, 6, "AA", b="BB", c="CC") 
+
+# # Example 06 (based on site ex)
+# def application(fname, lname, email, company, *args, **kwargs):
+#     print("{} {} works at {}. Her email is {}."\
+#           .format(fname, lname, company, email))
+#     print("{} {} works at {}. Her email is {}. args: {}, kwargs: {}"\
+#           .format(fname, lname, company, email, args, kwargs))
+
+# application("Jess", "Ingrass", "mail @ mail.com", "Teach Code", 75000, \
+#             hire_date = "September 2010")
+
+# # Output keyword arg follows all the positional ones
+# '''
+# Jess Ingrass works at Teach Code. Her email is mail @ mail.com.
+# Jess Ingrass works at Teach Code. Her email is mail @ mail.com. args: (75000,), kwargs: {'hire_date': 'September 2010'}
+# '''
