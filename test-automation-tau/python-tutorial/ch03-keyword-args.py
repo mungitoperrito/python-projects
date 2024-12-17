13e568e9ae75b43f2447bc82a940aa715a60b31c
Dave For Projects <daveforprojects@gmail.com>
2024-12-17 11:02:23 -0500

    ch03 args

---
+++ test-automation-tau/python-tutorial/ch03-keyword-args.py
@@ -0,0 +1,8 @@
+# https://testautomationu.applitools.com/python-tutorial/chapter3.html
+# Keyword args - scratch
+
+# Example01
+def user_info(name, age, city):
+    print("{} is {} years old, from {}".format(name, age, city))
+
+user_info("Name01", 42, "City01")
