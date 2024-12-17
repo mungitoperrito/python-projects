bfc46214b083af182be002b4444b1f2eb09c8d33
Dave For Projects <daveforprojects@gmail.com>
2024-12-17 10:56:10 -0500

    ch02 functions

--- test-automation-tau/python-tutorial/ch02..functions.py
+++ test-automation-tau/python-tutorial/ch02..functions.py
@@ -1,9 +1,21 @@
 # https://testautomationu.applitools.com/python-tutorial/chapter2.html
 # Functions - scratch
 
-def addition(num01, num02):
-    print("{} + {} = {}".format(num01, num02, num01 + num02))
+# Example 01
+# def addition(num01, num02):
+#     print("{} + {} = {}".format(num01, num02, num01 + num02))
 
-addition(3, 4)
-addition(3.0, 4)
-addition(0.3, 0.4)
+
+# addition(3, 4)
+# addition(3.0, 4)
+# addition(0.3, 0.4)
+
+
+# Example 02
+def addition():
+     num01 = int(input("Num 01: "))
+     num02 = int(input("Num 02: "))
+     print("{} + {} = {}".format(num01, num02, num01 + num02))
+
+
+addition()
