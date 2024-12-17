7b6f947c0c924b93b2f4e8cce70aed2beedad751
Dave For Projects <daveforprojects@gmail.com>
2024-12-17 10:51:44 -0500

    ch02 functions

---
+++ test-automation-tau/python-tutorial/ch02..functions.py
@@ -0,0 +1,9 @@
+# https://testautomationu.applitools.com/python-tutorial/chapter2.html
+# Functions - scratch
+
+def addition(num01, num02):
+    print("{} + {} = {}".format(num01, num02, num01 + num02))
+
+addition(3, 4)
+addition(3.0, 4)
+addition(0.3, 0.4)
--- test-automation-tau/python-tutorial/variables.py
+++
@@ -1,6 +0,0 @@
-# Variables intro
-
-car = 23
-CAR = 24
-print(f"car: {car}, CAR: {CAR}")
-print("car: {}, CAR: {}".format(car, CAR))
