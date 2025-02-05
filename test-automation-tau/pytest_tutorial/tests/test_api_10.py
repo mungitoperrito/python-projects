aa4264dc04313c430f5825261e1a3717796d5ccf
Dave For Projects <daveforprojects@gmail.com>
2025-02-04 20:08:02 -0500

    ch10 api

---
+++ test-automation-tau/pytest_tutorial/tests/test_api_10.py
@@ -0,0 +1,11 @@
+# ch10
+# https://testautomationu.applitools.com/pytest-tutorial/chapter10.html
+
+# For path inclusion, use: c> python -m pytest
+
+# # DEBUG
+# import sys
+# for p in sys.path:
+#   print(p)
+
+import pytest
