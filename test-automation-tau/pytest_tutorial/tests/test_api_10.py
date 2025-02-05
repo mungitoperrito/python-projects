5aeef4b3521e15a6f347f4e6fadc8b2d5fbf5cb5
Dave For Projects <daveforprojects@gmail.com>
2025-02-04 20:17:31 -0500

    add config file

--- test-automation-tau/pytest_tutorial/tests/test_api_10.py
+++ test-automation-tau/pytest_tutorial/tests/test_api_10.py
@@ -9,3 +9,18 @@
 #   print(p)
 
 import pytest
+import requests
+
+@pytest.mark.duckduckgo
+@pytest.mark.api
+def test_duck_duck_go_inst_ans_api():
+    # Setup
+    url = 'https://api.duckduckgo.com/?q=python+programming&format=json'
+
+    # Call
+    response = requests.get(url)
+    body = response.json()
+
+    # Assert
+    assert response.status_code == 200
+    assert 'Python' in body['AbstractText']
