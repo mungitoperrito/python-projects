4cf282a071128abd19e47773f1595a9791c88e32
Dave Cuthbert <daveforprojects@gmail.com>
2025-02-04 21:31:41 -0500

    ch11 playwright (#82)
    * :wqch11 playwright
    
    * playwright test code
    
    * playwright test code
    
    * playwright test code

---
+++ test_automation_tau/pytest_tutorial/tests/test_playwright_11.py
@@ -0,0 +1,30 @@
+# ch11
+# https://testautomationu.applitools.com/pytest-tutorial/chapter11.html
+
+# For path inclusion: python -m pytest
+# To run headed, with delays: python -m pytest --headed --slowmo 2000
+
+import pytest
+import re
+
+from playwright.sync_api import Page, expect
+
+@pytest.mark.ui
+@pytest.mark.acme_bank
+def test_acme_bank_login(page: Page):
+    # Setup
+    page.goto('https://demo.applitools.com/')
+
+    # Run
+    page.locator('id=username').fill('andy')
+    page.locator('id=password').fill('i<3pandas')
+    page.locator('id=log-in').click()
+
+    # Assert
+    expect(page.locator('div.logo-w')).to_be_visible()
+    expect(page.locator('ul.main-menu')).to_be_visible()
+    expect(page.get_by_text('Add Account')).to_be_visible()
+    expect(page.get_by_text('Make Payment')).to_be_visible()
+    expect(page.get_by_text('View Statement')).to_be_visible()
+    expect(page.get_by_text('Request Increase')).to_be_visible()
+    expect(page.get_by_text('Pay Now')).to_be_visible()
