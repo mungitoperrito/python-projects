b1e9846a7b29fdcd215b190c814c6fa2a303a035
Dave For Projects <daveforprojects@gmail.com>
2025-02-12 13:19:46 -0500

    class instantiate

--- oop-with-python/section_03.py
+++ oop-with-python/section_03.py
@@ -1,5 +1,7 @@
 ### Class methods v static methods
 
+import csv
+
 class Item:
     # Class attributes
     discount = 0.2      # price discount
@@ -24,6 +26,20 @@ class Item:
     def apply_discount(self):
         self.price = self.price * (1 - self.discount)
 
+    @classmethod
+    def import_from_csv(cls, csv_file):
+        with open(csv_file, 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            # print(item)             # Debug
+            Item(
+                name = item.get('name'),
+                price = float(item.get('price')),
+                quantity = int(item.get('quantity')),
+            )
+
     # Improve raw output when Item instances are printed
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
@@ -33,8 +49,11 @@ class Item:
 ### MAIN ###
 ############
 
-item_01 = Item("Phone", 100, 1)
-item_02 = Item("Compu", 1000, 3)
-item_03 = Item("Cable", 10, 5)
-item_04 = Item("Mouse", 50, 5)
-item_05 = Item("Keyboard", 75, 5)
+Item.import_from_csv('items.csv')
+
+# # Uncomment to debug
+# print(Item.all)
+
+# # Uncomment to debug
+# for i in Item.all:
+#     print(i)
