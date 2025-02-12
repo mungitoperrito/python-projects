44b6a43da9b666e4ab2659a9aa251c17c849856b
Dave For Projects <daveforprojects@gmail.com>
2025-02-12 00:45:37 -0500

    __repr__

--- oop-with-python/section_02.py
+++ oop-with-python/section_02.py
@@ -3,6 +3,7 @@
 class Item:
     # Class attributes
     discount = 0.2      # price discount
+    all = []            # list of all class instances
 
     def __init__(self, name: str, price: float, quantity=0):
         # Validate arguments
@@ -14,6 +15,9 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions on init
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
 
@@ -22,10 +26,30 @@ class Item:
         # Use self.discount instead of Item.discount so individual
         #    instances can override the value
 
-item_01 = Item("Phone", 100, 5)
-item_02 = Item("Compu", 1000, 3 )
-# item_03 = Item("FailPrice", -50, 3 )      # Uncomment to test assert
-# item_04 = Item("FailQuantity", 50, -3 )   # Uncomment to test assert
+    # Improve raw output when Item instances are printed
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
+        # Before
+        # <__main__.Item object at 0x000001D453607050>
+        # After
+        # Item('Phone', 100, 1)
+
+item_01 = Item("Phone", 100, 1)
+item_02 = Item("Compu", 1000, 3)
+item_03 = Item("Cable", 10, 5)
+item_04 = Item("Mouse", 50, 5)
+item_05 = Item("Keyboard", 75, 5)
+
+# # Uncomment to see list of all instances (before __repr__)
+# for i in Item.all:
+#     print(i.name)
+
+# Uncomment to see list of all instances
+print(Item.all)
+
+# # Uncomment to test asserts
+# item_03 = Item("FailPrice", -50, 3 )
+# item_04 = Item("FailQuantity", 50, -3 )
 
 # # Uncomment to show class variable
 # print(Item.discount)
