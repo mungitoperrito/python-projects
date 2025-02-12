c7a0de362e99f41842743fadc9dd5193326d97a4
Dave For Projects <daveforprojects@gmail.com>
2025-02-12 13:47:50 -0500

    better test

--- oop-with-python/section_04.py
+++ oop-with-python/section_04.py
@@ -26,6 +26,8 @@ class Item:
     def apply_discount(self):
         self.price = self.price * (1 - self.discount)
 
+
+    # First paramter is class(cls)
     @classmethod
     def import_from_csv(cls, csv_file):
         with open(csv_file, 'r') as f:
@@ -39,6 +41,26 @@ class Item:
                 price = float(item.get('price')),
                 quantity = int(item.get('quantity')),
             )
+
+    # First paramter is neither instance(self) nor class(cls)
+    @staticmethod
+    def is_integer(number):
+        # CAUTION: is_integer considers x.0 to be an int
+        # if isinstance(number, float):
+        #     return number.is_integer()
+        # elif isinstance(number, int):
+        #     return True
+        # else:
+        #     return False
+
+        # Should not return True for x.0
+        if isinstance(number, float):
+            return False
+        elif isinstance(number, int):
+            return True
+        else:
+            return False
+
 
     # Improve raw output when Item instances are printed
     def __repr__(self):
@@ -57,3 +79,10 @@ Item.import_from_csv('items.csv')
 # # Uncomment to debug
 # for i in Item.all:
 #     print(i)
+
+if __name__ == "__main__":
+
+    # Uncomment to test is_integer static method
+    for value in [5, 5.0, 5.1]:
+        print(f"{value}: {Item.is_integer(value)}")
+
