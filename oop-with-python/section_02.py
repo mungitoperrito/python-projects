b117bdb4462d12d1e2b92c9c69a8595474f73067
Dave For Projects <daveforprojects@gmail.com>
2025-02-12 00:12:12 -0500

    class variable

--- oop-with-python/section_02.py
+++ oop-with-python/section_02.py
@@ -1,23 +1,28 @@
 ### Intro to __init__
 
 class Item:
-    def __init__(self, name: str, price: float, quantity=0):
+    # Class attributes
+    discount = 0.2      # price discount
 
-        # Validate arguemnts
+    def __init__(self, name: str, price: float, quantity=0):
+        # Validate arguments
         assert price > 0, f"Price {price} is not greater than 0"
         assert quantity >= 0, f"Quantity {quantity} is not greater than 0"
 
+        # Instance attributes
         self.name = name
         self.price = price
         self.quantity = quantity
 
     def calculate_total_price(self):
-        return self.price * self.quantity
+        return self.price * self.quantity * (1 - self.discount)
 
 item_01 = Item("Phone", 100, 5)
 item_02 = Item("Compu", 1000, 3 )
 # item_03 = Item("FailPrice", -50, 3 )      # Uncomment to test assert
 # item_04 = Item("FailQuantity", 50, -3 )   # Uncomment to test assert
+
+print(Item.discount)
 
 print(item_01.calculate_total_price())
 print(item_02.calculate_total_price())
