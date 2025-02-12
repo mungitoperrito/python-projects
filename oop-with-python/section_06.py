7fc3f9332950208c14e9f077faad767d2a6b0f27
Dave Cuthbert <daveforprojects@gmail.com>
2025-02-12 17:28:29 -0500

    refactor into classes (#92)
    * refactor into classes
    
    * encapsulation

---
+++ oop-with-python/section_06.py
@@ -0,0 +1,21 @@
+### refactor, encapsulation, getter and setters
+
+from item import Item
+from phone import Phone
+
+############
+### MAIN ###
+############
+
+if __name__ == "__main__":
+
+    # Uncomment to initialize items
+    Item.import_from_csv('items.csv')
+
+    item_01 = Item("New thing", 750)
+    print(item_01.name)
+
+    # Uncomment to debug
+    print(f"Item: {Item.all}")
+
+
