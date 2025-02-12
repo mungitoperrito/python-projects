7b193623fc6014d257884e124162f7f51de6d308
Dave For Projects <daveforprojects@gmail.com>
2025-02-12 00:29:27 -0500

    class variable overide

--- oop-with-python/section_02.py
+++ oop-with-python/section_02.py
@@ -15,20 +15,43 @@ class Item:
         self.quantity = quantity
 
     def calculate_total_price(self):
-        return self.price * self.quantity * (1 - self.discount)
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * (1 - self.discount)
+        # Use self.discount instead of Item.discount so individual
+        #    instances can override the value
 
 item_01 = Item("Phone", 100, 5)
 item_02 = Item("Compu", 1000, 3 )
 # item_03 = Item("FailPrice", -50, 3 )      # Uncomment to test assert
 # item_04 = Item("FailQuantity", 50, -3 )   # Uncomment to test assert
 
-# Show class variable
-print(Item.discount)
+# # Uncomment to show class variable
+# print(Item.discount)
 
-print(item_01.calculate_total_price())
-print(item_02.calculate_total_price())
+# # Uncomment to show method working
+# print(item_01.calculate_total_price())
+# print(item_02.calculate_total_price())
 
-# Show all variables
-print(Item.__dict__)
-print(item_01.__dict__)
+# # Uncomment to show all variables
+# print(Item.__dict__)            # Class level
+# print(item_01.__dict__)         # Instance level
 
+# # # Uncomment to overide class variable on instance level
+# remember_price_01 = item_01.price
+# item_01.apply_discount()
+# print(item_01.calculate_total_price())
+
+# item_01.price = remember_price_01
+# item_01.discount = 0.5
+# item_01.apply_discount()
+# print(item_01.calculate_total_price())
+
+# remember_price_02 = item_02.price
+# item_02.apply_discount()
+# print(item_02.calculate_total_price())
+
+# item_02.price = remember_price_02
+# item_02.apply_discount()
+# print(item_02.calculate_total_price())
