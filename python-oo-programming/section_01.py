f7061f542356462934cc8a170c91132809f5570f
Dave For Projects <daveforprojects@gmail.com>
2025-02-13 21:56:18 -0500

    section 01

--- python-oo-programming/section_01.py
+++ python-oo-programming/section_01.py
@@ -13,9 +13,25 @@
 ##############
 # Simple classes
 
+# Class def
 class Dog:
+    # Attributes
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+
+    # Method def
     def bark(self):
         print("Woof woof")
 
-dog_01 = Dog()
-dog_01.bark()
+# Create some dog objects
+dog_01 = Dog("Bowser", "Terrier")
+dog_02 = Dog("Firulais", "Mutt")
+
+# # Uncomment to test methods
+# dog_01.bark()
+# dog_02.bark()
+
+# View attributes
+for dog in [dog_01, dog_02]:
+    print(f"{dog.name} is a {dog.breed}")
