3780bda0ce462db2cd123efd7bb3158e862dad52
Dave For Projects <daveforprojects@gmail.com>
2025-02-13 21:49:44 -0500

    section 01

---
+++ python-oo-programming/section_01.py
@@ -0,0 +1,13 @@
+##############
+
+# # Everything is an object. Objects come from classes
+# name = "Dave"
+# age = 18
+
+# print(type(name.lower()))
+# print(type(age))
+
+# print(name)
+# print(name.upper())
+
+##############
