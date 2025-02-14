31ec4d67d40fff86e60030025b75876346f30e23
Dave For Projects <daveforprojects@gmail.com>
2025-02-13 22:11:00 -0500

    Example

--- python-oo-programming/section_01.py
+++ python-oo-programming/section_01.py
@@ -16,22 +16,34 @@
 # Class def
 class Dog:
     # Attributes
-    def __init__(self, name, breed):
+    def __init__(self, name, breed, owner=''):
         self.name = name
         self.breed = breed
+        self.owner = owner
 
     # Method def
     def bark(self):
         print("Woof woof")
 
+class Owner:
+    def __init__(self, name, address='', number=''):
+        self.name = name
+        self.address = address
+        self.phone_number = number
+
+
+# Creat some owners
+owner_01 = Owner("Dave")
+owner_02 = Owner("Janet")
+
 # Create some dog objects
-dog_01 = Dog("Bowser", "Terrier")
-dog_02 = Dog("Firulais", "Mutt")
+dog_01 = Dog("Bowser", "Terrier", owner_02)
+dog_02 = Dog("Firulais", "Mutt", owner_01)
 
 # # Uncomment to test methods
-# dog_01.bark()
-# dog_02.bark()
+# for dog in [dog_01, dog_02]:
+#   dog.bark()
 
 # View attributes
 for dog in [dog_01, dog_02]:
-    print(f"{dog.name} is a {dog.breed}")
+    print(f"{dog.name} is a {dog.breed}, owner is: {dog.owner.name}")
