9dba97d4df131a84c5849501e6b7309d77bd161c
Dave For Projects <daveforprojects@gmail.com>
2025-02-15 12:40:18 -0500

    Abstraction

--- python-oo-programming/section_09.py
+++ python-oo-programming/section_09.py
@@ -1,2 +1,31 @@
 # Abstraction
 
+class EmailServer:
+
+    def _connect(self):
+        print('Connecting ...')
+
+
+    def _authenticate(self):
+        print('Authenticating ...')
+
+
+    def _disconnect(self):
+        print('Disconnecting')
+
+
+    def send_email(self):
+        self._connect()
+        self._authenticate()
+        print('Sending email ...')
+        self._disconnect()
+
+
+############
+### MAIN ###
+############
+
+if __name__ == '__main__':
+    email_01 = EmailServer()
+    email_01.send_email()
+    # EmailServer.send_email()   # Fails, has to be @staticmethod, @classmethod
