fdae05a27d59722293c6b8752edadf13a3522da8
Dave Cuthbert <daveforprojects@gmail.com>
2025-02-14 19:21:00 -0500

    Protect attributes (#98)
    * Protect attributes
    
    * Protect attributes
    
    * Protect attributes

--- python-oo-programming/section_02.py
+++ python-oo-programming/section_02.py
@@ -6,9 +6,9 @@ class Person:
     def greet(self):
         print(f"{self.name} is {self.age}")
 
-# Uncomment to test
-person_01 = Person("Dave", 100)
-person_02 = Person("Bob", 50)
+# # Uncomment to test
+# person_01 = Person("Dave", 100)
+# person_02 = Person("Bob", 50)
 
-for p in [person_01, person_02]:
-  p.greet()
+# for p in [person_01, person_02]:
+#   p.greet()
