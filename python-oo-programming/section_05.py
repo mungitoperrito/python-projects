769356eb5e2b9853228c857d12dc97be7b8939e4
Dave For Projects <daveforprojects@gmail.com>
2025-02-14 20:31:06 -0500

    Properties

--- python-oo-programming/section_05.py
+++ python-oo-programming/section_05.py
@@ -3,10 +3,14 @@
 # Protected by convention, single underscore
 class User:
     def __init__(self, username, email, password):
-        self.username = username
+        self._username = username
         self._email = email
-        self.password = password
+        self._password = password
 
+    @property
+    def email(self):
+        print("In property setup")
+        return self._email
 
     def get_email(self):
         return self._email
@@ -18,5 +22,4 @@ user_01 = User("Dave", "aaa@corp.com", "abc")
 user_02 = User("Bob", "bbb@corp.com", "abc")
 
 for u in [user_01, user_02]:
-    u.set_email('ccc@corp.com')
-    print(u.get_email())
+    print(u.email)
