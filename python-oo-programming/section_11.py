989a489e261fb013f41c54262741d99f869e0188
Dave For Projects <daveforprojects@gmail.com>
2025-02-15 14:33:44 -0500

    Polymorphism

--- python-oo-programming/section_11.py
+++ python-oo-programming/section_11.py
@@ -1,10 +1,12 @@
 # Polymorphism
 
-class Vehicle:
-    def __init__(self, brand, model, year):
+class Car:
+    def __init__(self, brand, model, year, doors, wheels):
         self.brand = brand
         self.model = model
         self.year = year
+        self.doors = doors
+        self.wheels = wheels
 
 
     def start(self):
@@ -15,17 +17,18 @@ class Vehicle:
         print(f'{self.__class__.__name__}: is stopping' )
 
 
-class Car(Vehicle):
-    def __init__(self, brand, model, year, doors, wheels):
-        super().__init__(brand, model, year)
-        self.doors = doors
-        self.wheels = wheels
-
-
-class Bike(Vehicle):
+class Motorcycle:
     def __init__(self, brand, model, year, wheels):
-        super().__init__(brand, model, year)
-        self.wheels = wheels
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def start(self):
+        print(f'{self.__class__.__name__}: is starting' )
+
+
+    def stop(self):
+        print(f'{self.__class__.__name__}: is stopping' )
 
 
 ############
@@ -35,19 +38,21 @@ class Bike(Vehicle):
 if __name__ == '__main__':
 
     # # Uncomment to test
-    # vehicle_01 = Vehicle('Brand_A', 'Model_A', 1910)
-    # vehicle_01.start()
-    # vehicle_01.stop()
+    # car_01 = Car('Brand_C', 'Model_C', 2020, 3, 4)
+    # print(car_01.__dict__)
+    # car_01.start()
+
+    # # Uncomment to test
+    # bike_01 = Motorcycle('Brand_M', 'Model_M', 2010, 2)
+    # print(bike_01.__dict__)
+    # bike_01.start()
 
     # Uncomment to test
-    car_01 = Car('Brand_B', 'Model_B', 2020, 3, 4)
-    print(car_01.__dict__)
-    car_01.start()
-    car_01.stop()
-
-    # Uncomment to test
-    bike_01 = Bike('Brand_C', 'Model_C', 2010, 2)
-    print(bike_01.__dict__)
-    bike_01.start()
-    bike_01.stop()
-
+    vehicles = [
+        Car('BrandCC', 'ModelCC', 2018, 4, 4),
+        Motorcycle('BrandMM', 'ModelMM', 2021, 2)
+    ]
+    for v in vehicles:
+        print(v.__dict__)
+        v.start()
+        v.stop()
