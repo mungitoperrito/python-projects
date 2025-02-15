e9e43239113c0af7d73ff09ea721c44ac8db0050
Dave For Projects <daveforprojects@gmail.com>
2025-02-14 19:20:09 -0500

    Protect attributes

--- python-oo-programming/section_04.py
+++ python-oo-programming/section_04.py
@@ -1,18 +1,34 @@
-# Protected, but only by convention
+# # Protected, but only by convention
+# class User:
+#     def __init__(self, username, email, password):
+#         self.username = username
+#         self._email = email
+#         self.password = password
+
+#     def clean_email(self):
+#        return self._email.lower().strip()
+
+# user_01 = User("Dave", "aaa@corp.com", "abc")
+# user_02 = User("Bob", "  BBB@corp.com ", "abc")
+
+# for u in [user_01, user_02]:
+#   print(u._email)           # Breaks convention, but works
+#   print(u.clean_email())    # Expected way
+
+
+# Actually protected (Private), use double underscore
 class User:
     def __init__(self, username, email, password):
         self.username = username
-        self._email = email
+        self.__email = email
         self.password = password
 
     def clean_email(self):
-       return self._email.lower().strip()
+       return self.__email.lower().strip()
 
 user_01 = User("Dave", "aaa@corp.com", "abc")
 user_02 = User("Bob", "  BBB@corp.com ", "abc")
 
 for u in [user_01, user_02]:
-  print(u._email)           # Breaks convention, but works
+  # print(u.__email)          # Doesn't work
   print(u.clean_email())    # Expected way
-
-
