65d4ce8d1ac489d60e3d7cf1ba4be340a859ead1
Dave For Projects <daveforprojects@gmail.com>
2025-02-15 20:15:11 -0500

    Historias locas

--- seis-proyectos-python/historias-locas.py
+++ seis-proyectos-python/historias-locas.py
@@ -13,21 +13,20 @@ def get_words(word_types):
 
     return word_list
 
-def print_mad_lib(word_list, skeleton_text):
-    pass
+def print_mad_lib(word_list):
+        print(f'Programar es tan {word_list['adjectivo_01']}. ', end='')
+        print(f'Siempre me {word_list['verbo_01']} resolver ', end='')
+        print(f'problemas. Aprende a {word_list['verbo_02']} ', end='')
+        print(f'con {word_list['organizacion_01']} y alcanza ', end='')
+        print(f'tus {word_list['sustantivo-plural_01']}.')
 
 
 ############
 ### MAIN ###
 ############
 
-skeleton_text = ' Programar es tan {word_list[adjectivo_01]}. \
-                Siempre me {word_list[verbo_01]} resolver problemas. \
-                Aprende a {word_list[verbo_02]} con {organizacion_01} \
-                y alcanza tu {word_list[sustantivo_01]}.'
-
 word_types = ['adjectivo_01', 'verbo_01', 'verbo_02', 'organizacion_01',
-              'sustantivo_01' ]
+              'sustantivo-plural_01' ]
 
 word_list = get_words(word_types)
-print_mad_lib(word_list, skeleton_text)
+print_mad_lib(word_list)
