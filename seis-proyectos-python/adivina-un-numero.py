7783b326f1071999ee9901eaa7d7e45fa737a8db
Dave For Projects <daveforprojects@gmail.com>
2025-02-16 16:47:34 -0500

    Adivina el numero

--- seis-proyectos-python/adivina-un-numero.py
+++ seis-proyectos-python/adivina-un-numero.py
@@ -1,5 +1,19 @@
 import random
 
+def imprimir_saludo():
+    print()
+    print('#####################')
+    print('# Adivina un número #')
+    print('#####################')
+    print()
+    print('Para salir, entra -1')
+    print()
+
+def salir(valor):
+    if valor == -1:
+        exit()
+
+
 def obtener_valor_mayor():
     try:
         valor_mayor = int(input('Entra un numero positivo: '))
@@ -8,11 +22,15 @@ def obtener_valor_mayor():
         print('Entra un numero entero positivo como: 13')
         valor_mayor = obtener_valor_mayor()
 
+    # Exit if -1
+    salir(valor_mayor)
+
     # Check if  0 or negative
     if (valor_mayor < 1):
         valor_mayor = obtener_valor_mayor()
 
     return valor_mayor
+
 
 def obtener_valor(valor_mayor):
     try:
@@ -21,33 +39,37 @@ def obtener_valor(valor_mayor):
     except:
         # Retry if not convirtable to int type
         print('Entra un numero entero positivo como: 5')
-        valor_mayor = obtener_valor(valor_mayor)
+        valor = obtener_valor(valor_mayor)
+
+    # Exit if -1
+    salir(valor)
 
     # Check if value is in range
     if (valor > valor_mayor) or (valor < 1):
         valor = obtener_valor(valor_mayor)
 
-    return valor_mayor
+    return valor
+
 
 def adivina_el_numero(valor_mayor):
-    adivinacion = 0
+    adivina = 0
     valor = random.randint(0, valor_mayor)
-    print(f'DEBUG: {valor}')
+    # print(f'DEBUG: {valor}')
     cuenta = 0
 
-    while adivinacion != valor:
+    while adivina != valor:
         cuenta += 1
-        adivinacion = obtener_valor(valor_mayor)
+        adivina = obtener_valor(valor_mayor)
 
-        if adivinacion < valor:
+        if adivina < valor:
             print('Demasiado bajo')
 
-        if adivinacion > valor:
+        if adivina > valor:
             print('Demasiado alto')
 
 
     print('Ganaste!')
-    print(f'Número de adivinancas: {cuenta}')
+    print(f'Número de intentos: {cuenta}')
 
 
 
@@ -55,6 +77,6 @@ def adivina_el_numero(valor_mayor):
 ### MAIN ###
 ############
 
-print('Adivina un número.')
+imprimir_saludo()
 valor_mayor = obtener_valor_mayor()
 adivina_el_numero(valor_mayor)
