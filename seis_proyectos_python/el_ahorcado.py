e390f198327339e970d7f6bd25ef73d7db8dd848
Dave For Projects <daveforprojects@gmail.com>
2025-02-22 03:24:38 -0500

    El ahorcado

--- seis_proyectos_python/el_ahorcado.py
+++ seis_proyectos_python/el_ahorcado.py
@@ -1,11 +1,14 @@
 # Juego del ahorcado
 
 import random
-import sys
+
+from dibujos import dibujos
+
 
 # Uncomment as needed to test or run
 # LISTA_DE_PALABRAS = "lista_de_palabras.txt"
 LISTA_DE_PALABRAS = "lista_de_palabras_test.txt"
+
 
 def imprimir_saludo():
     print()
@@ -18,6 +21,9 @@ def imprimir_saludo():
 
 
 def escoger_palabra():
+    # Compare explicit file open and process with python import method
+    #    for importing the drawings
+
     lista_de_palabras = []
 
     try:
@@ -30,13 +36,55 @@ def escoger_palabra():
         print(f"Error leyendo el archivo, {LISTA_DE_PALABRAS}.")
 
     length = len(lista_de_palabras)
-    index = random.randint(0, length)
+    index = random.randint(0, length - 1)
     palabra = lista_de_palabras[index]
 
     return palabra
 
-def jugar():
+
+def update_palabra(palabra_letras, palabra, letra):
+    index = 0
+
+    while True
+        index = palabra.find(letra, index)
+        if index == -1:
+            break
+        else:
+            palabra_letras[index] = letra
+            index += 1
+
+    return palabra_letras
+
+
+def jugar(palabra):
     rondas = 0
+    palabra = palabra.upper()
+    palabra_letras = ['_' for x in list(palabra)]
+    letras_adivinadas = set()
+    vidas = 6
+    print(palabra) # DEBUG
+    # print(palabra_letras) # DEBUG
+
+    while vidas > 0:
+        rondas += 1
+
+        letra = obtener_letra()
+        if letra in letras_adivinadas:
+            print(f"Ya advinaste {letra}")
+            continue
+        else:
+            letras_adivinadas.add(letra)
+
+        if letra in palabra:
+            palabra_letras = update_palabra(palabra_letras, palabra, letra)
+            print(palabra_letras)
+        else:
+            vidas -= 1
+
+        # Update info for this round
+        print("\n")
+        print(f"Adivinada: {sorted(letras_adivinadas)}")
+        print(dibujos[vidas])
 
 
 def obtener_letra():
@@ -51,4 +99,13 @@ def obtener_letra():
 
     # No error handling for non-letters
 
-    return letra
+    return letra.upper()
+
+
+############
+### MAIN ###
+############
+
+imprimir_saludo()
+palabra = escoger_palabra()
+jugar(palabra)
