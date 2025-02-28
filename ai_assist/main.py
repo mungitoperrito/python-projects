a3c62b089f0f3518b2e30e386f201b475198dee1
Dave For Projects <daveforprojects@gmail.com>
2025-02-28 18:47:10 -0500

    functions

--- ai_assist/main.py
+++ ai_assist/main.py
@@ -1,18 +1,37 @@
+import pyperclip
+import time
+
 from pynput import keyboard
 from pynput.keyboard import Key, Controller
 
 # CONSTANTS
-F8 = str(Key.f8.value)
-F9 = str(Key.f9.value)
-F10 = str(Key.f10.value)
-
+F8 = str(Key.f8.value)    # For exit
+F9 = str(Key.f9.value)    # For fix line
+F10 = str(Key.f10.value)  # For fix selection
+SLEEP_DELAY = 0.3         # Allow for data transfer
 
 def fix_current_line():
     pass
 
 
 def fix_selection():
-    pass
+
+    # Step 1 - copy text to clipboard
+    with controller.pressed(Key.ctrl):
+        controller.tap('c')
+
+    # Step 2 - get the text from the clipboard
+    time.sleep(SLEEP_DELAY)
+    text = pyperclip.paste()
+
+    # Step 3 -
+
+    # Step 4 - copy the text to the clipboard
+    pyperclip.paste(text)
+
+    # Step 5 - paste text into the doc
+    with controller.pressed(Key.ctrl):
+        controller.tap('v')
 
 
 def on_f8():
@@ -35,6 +54,8 @@ def on_f10():
 ### MAIN ###
 ############
 
+# Create keyboard control object
+controller = Controller()
 
 with keyboard.GlobalHotKeys({
         F8 : on_f8,
