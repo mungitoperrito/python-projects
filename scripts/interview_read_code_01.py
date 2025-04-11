c5031b006ce0c2b83d428de30cc7defc43d86f93
Dave For Projects <daveforprojects@gmail.com>
2025-04-11 18:40:52 -0400

    Sample output

--- scripts/interview_read_code_01.py
+++ scripts/interview_read_code_01.py
@@ -26,4 +26,18 @@ print(f"Mystery function 02: {mystery_function_02(input_list)}")
 Both functions print out pairs of values.
 mystery_function_01: prints (a,b) and (b,a)
 mystery_function_02: only prints (a,b)
+
+Mystery function 01:
+[('a', 'a'), ('a', 'b'), ('a', 'c'), ('a', 'd'), ('a', 'e'),
+   ('b', 'a'), ('b', 'b'), ('b', 'c'), ('b', 'd'), ('b', 'e'),
+     ('c', 'a'), ('c', 'b'), ('c', 'c'), ('c', 'd'), ('c', 'e'),
+       ('d', 'a'), ('d', 'b'), ('d', 'c'), ('d', 'd'), ('d', 'e'),
+         ('e', 'a'), ('e', 'b'), ('e', 'c'), ('e', 'd'), ('e', 'e')]
+
+Mystery function 02:
+[('a', 'a'), ('a', 'b'), ('a', 'c'), ('a', 'd'), ('a', 'e'),
+             ('b', 'b'), ('b', 'c'), ('b', 'd'), ('b', 'e'),
+                         ('c', 'c'), ('c', 'd'), ('c', 'e'),
+                                     ('d', 'd'), ('d', 'e'),
+                                                 ('e', 'e')]
 '''
