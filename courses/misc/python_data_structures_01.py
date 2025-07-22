53edd9f6e3041489d660b3c09aea6fe376c69618
Dave For Projects <daveforprojects@gmail.com>
2025-07-21 23:01:59 -0400

    intro

---
+++ courses/misc/python_data_structures_01.py
@@ -0,0 +1,12 @@
+'''
+Source:
+
+https://www.dataquest.io/blog/data-structures-in-python/
+'''
+
+# 01
+list_01 = ['a', 'b', 'c']
+list_01.append('d')
+print(f"list_01 {list_01}")
+list_01.remove('c')            # Any element, not just pop
+print(f"list_01 {list_01}")
