2ad891f39e155740be641009a74a146f9ffcec7a
Dave For Projects <daveforprojects@gmail.com>
2025-10-07 12:48:25 -0400

    updateto argparse

--- books/violent_python/port_scan/port_scan.py
+++ books/violent_python/port_scan/port_scan.py
@@ -1,12 +1,13 @@
-import optparse
+import argparse
 
 def get_opts():
-    parser = optparse.OptionParser('Usage %prog -H  <target_host> -p  <target_port>')
-    parser.add_option('-H', dest='tgt_host', type='string', help='Target host')
-    parser.add_option('-p', dest='tgt_port', type='int', help='Target port')
-    (options, args) = parser.parse_args()
-    target_host = options.tgt_host
-    target_port = options.tgt_port
+    parser = argparse.ArgumentParser(description="scan ports")
+    parser.usage="%prog --host  <target_host> --port  <target_port>"
+    parser.add_argument('-H', '--host', type=str, help='Target host')
+    parser.add_argument('-p', '--port', type=int, help='Target port')
+    args = parser.parse_args()
+    target_host = args.host
+    target_port = args.port
 
     if ((target_host == None) | (target_port == None)):
         print(parser.usage)
