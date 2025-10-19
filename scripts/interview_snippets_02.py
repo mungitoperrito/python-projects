ecf7b20a9b6475550bbea2bc8064b8c21e04bdde
Dave For Projects <daveforprojects@gmail.com>
2025-10-18 21:24:15 -0400

    palindrome check

---
+++ scripts/interview_snippets_02.py
@@ -0,0 +1,12 @@
+########################################################################
+# QUESTION: Check if a string is a palindrome                          #
+########################################################################
+
+# def is_palindrome(str_to_check):
+#     reversed = str_to_check[::-1]
+#     return reversed == str_to_check
+
+# for s in ['abc', 'fghjhgf', '', 'aab']:
+#     print(f"string: {s} palindrome? {is_palindrome(s)}")
+
+#     # Needs a check for empty string edge case
