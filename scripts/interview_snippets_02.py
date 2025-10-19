e7362eb2d959d72b9aee5c34e328c6b71a311f7e
Dave Cuthbert <daveforprojects@gmail.com>
2025-10-18 21:43:04 -0400

    In05 (#167)
    * palindrome check
    
    * factorial

--- scripts/interview_snippets_02.py
+++ scripts/interview_snippets_02.py
@@ -10,3 +10,29 @@
 #     print(f"string: {s} palindrome? {is_palindrome(s)}")
 
 #     # Needs a check for empty string edge case
+
+
+########################################################################
+# QUESTION: Find the factorial of a positive integer                   #
+########################################################################
+
+# def factorial_01(n):
+#     factorial = 1
+#     while n > 0:
+#         factorial *= n
+#         n -= 1
+
+#     return factorial
+
+# # Recursive function
+# def factorial_02(n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return n * factorial_02(n-1)
+
+# # Check the function
+# for n in [0, 3, 10]:
+#     # 0! == 1 by definition
+#     # Correct values: 1, 6, 3628800
+#     print(f"n: {n}  factorial: {factorial_02(n)}")
