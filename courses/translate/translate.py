bb280a635a39ae758237dfa2943b15b7a78b1d61
Dave Cuthbert <daveforprojects@gmail.com>
2025-10-22 23:48:36 -0400

    translate (#172)

--- courses/translate/translate.py
+++ courses/translate/translate.py
@@ -4,3 +4,46 @@ import speech_recognition as sr
 
 from googletrans import Translator
 from gtts import gTTS # Google Text-to-Speech
+
+# # Uncomment to list languages
+# print(gt.LANGUAGES)
+# print(f"number of lanugages: {len(gt.LANGUAGES)}")
+
+
+# Configure audio
+mic = sr.Microphone()
+rec = sr.Recognizer()
+
+
+# Capture audio
+with mic as source:
+    # Initialize the translator
+    translator = Translator()
+
+    # Define input and output languages
+    input_lang = 'en'   # English
+    output_lang = 'es'    # Spanish
+
+    # Prompt for speech
+    print("Talk now.")
+
+    # Calibrate recording threshold
+    rec.adjust_for_ambient_noise(source, duration=0.2)
+
+    # Listen until there is silence
+    audio = rec.listen(source)
+
+# Detect words in audio clip
+recorded_audio = rec.recognize_google(audio)
+# Uncomment to debug
+print(f"RECORDED: {recorded_audio}")
+
+
+# # Detect the language
+lang = translator.detect(recorded_audio)
+print(f"LANG: {lang}")
+
+# Translate the text
+data = translator.translate(recorded_audio, src=input_lang, dest=output_lang)
+text_translated = data.text
+print(f"TRANSLATED: {text_translated}")
